f4169fe32d91b8177e9516e4cc0672dbb0f07343
Isidro López <islomar@gmail.com>
2018-06-01 18:15:44 +0200

    [test] Renaming HellowWorldXxx to PaymentsApiXxx

--- src/test/java/com/islomar/payments_api/end2end/PaymentsApiIntegrationTest.java
+++ src/test/java/com/islomar/payments_api/end2end/PaymentsApiIntegrationTest.java
@@ -7,6 +7,6 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin = { "pretty", "html:target/cucumber", "junit:target/junit-report.xml" },
-        features = "src/test/resources/features/hello-world.feature")
-public class HelloWorldIntegrationTest {
+        features = "src/test/resources/features/")
+public class PaymentsApiIntegrationTest {
 }
--- src/test/java/com/islomar/payments_api/end2end/steps/PaymentsApiStepDefinitions.java
+++ src/test/java/com/islomar/payments_api/end2end/steps/PaymentsApiStepDefinitions.java
@@ -1,6 +1,7 @@
 package com.islomar.payments_api.end2end.steps;
 
 import com.islomar.payments_api.end2end.SpringBootBaseFeatureTest;
+import cucumber.api.PendingException;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.When;
 import cucumber.api.java.en.Then;
@@ -10,7 +11,7 @@ import org.springframework.http.ResponseEntity;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
-public class HelloWorldStepDefinitions extends SpringBootBaseFeatureTest {
+public class PaymentsApiStepDefinitions extends SpringBootBaseFeatureTest {
 
     private ResponseEntity<String> response;
 
@@ -27,5 +28,12 @@ public class HelloWorldStepDefinitions extends SpringBootBaseFeatureTest {
     @And("^the client receives response body text \"([^\"]*)\"$")
     public void theClientReceivesResponseBodyText(String text) throws Throwable {
         assertThat(this.response.getBody(), is(text));
+    }
+
+    @When("the client calls GET \\/payments\\/{int}")
+    public void the_client_calls_GET_payments(Integer paymentId) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+
     }
 }
