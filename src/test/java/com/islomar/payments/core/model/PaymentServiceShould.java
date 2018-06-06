d545b79e621dc28a6fef5d493f6d356df8a21200
Isidro López <islomar@gmail.com>
2018-06-06 18:46:20 +0200

    [test] Implement some missing isolated tests for the Service

--- src/test/java/com/islomar/payments/core/model/PaymentServiceShould.java
+++ src/test/java/com/islomar/payments/core/model/PaymentServiceShould.java
@@ -1,14 +1,20 @@
 package com.islomar.payments.core.model;
 
+import com.islomar.payments.core.model.exceptions.PaymentNotFoundException;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 
-import java.util.UUID;
+import java.util.*;
 
-import static com.islomar.payments.shared.ObjectMother.aDummyPayment;
+import static com.islomar.payments.shared.ObjectMother.*;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 import static org.mockito.MockitoAnnotations.initMocks;
 
@@ -25,7 +31,7 @@ public class PaymentServiceShould {
     }
 
     @Test
-    public void save_a_valid_payment() {
+    public void persist_the_payment_when_saving_a_payment() {
         Payment dummyPayment = aDummyPayment();
 
         this.paymentService.save(dummyPayment);
@@ -34,7 +40,7 @@ public class PaymentServiceShould {
     }
 
     @Test
-    public void return_paymentId_with_UUID_format() {
+    public void return_paymentId_with_UUID_format_when_saving_a_payment() {
         Payment dummyPayment = aDummyPayment();
 
         Payment createdPayment = this.paymentService.save(dummyPayment);
@@ -43,12 +49,76 @@ public class PaymentServiceShould {
     }
 
     @Test
-    public void update_a_valid_payment() {
-        Payment dummyPayment = aDummyPayment();
+    public void persist_the_payment_when_updating_an_existing_payment() {
+        Payment existingPayment = aPaymentBuilder().id(ANY_VALID_PAYMENT_ID).build();
+        given(paymentRepository.findById(ANY_VALID_PAYMENT_ID)).willReturn(Optional.of(existingPayment));
 
-        this.paymentService.save(dummyPayment);
+        existingPayment.setOrganisationId("another-organisation-id");
+        this.paymentService.update(ANY_VALID_PAYMENT_ID, existingPayment);
 
-        verify(this.paymentRepository).save(any(Payment.class));
+        verify(this.paymentRepository).update(any(Payment.class));
+    }
+
+    @Test(expected = PaymentNotFoundException.class)
+    public void throw_PaymentNotFoundException_when_updating_a_non_existing_payment() {
+        given(paymentRepository.findById(ANY_NON_EXISTING_PAYMENT_ID)).willReturn(Optional.empty());
+
+        Payment paymentToBeUpdated = aPaymentBuilder().id(ANY_NON_EXISTING_PAYMENT_ID).build();
+        this.paymentService.update(ANY_NON_EXISTING_PAYMENT_ID, paymentToBeUpdated);
+    }
+
+    @Test
+    public void delete_a_payment_if_it_exists() {
+        given(paymentRepository.findById(ANY_VALID_PAYMENT_ID)).willReturn(Optional.of(aDummyPayment()));
+
+        this.paymentService.delete(ANY_VALID_PAYMENT_ID);
+
+        verify(this.paymentRepository).deleteById((ANY_VALID_PAYMENT_ID));
+    }
+
+    @Test(expected = PaymentNotFoundException.class)
+    public void throw_PaymentNotFoundException_when_deleting_a_non_existing_payment() {
+        given(paymentRepository.findById(ANY_NON_EXISTING_PAYMENT_ID)).willReturn(Optional.empty());
+
+        this.paymentService.delete(ANY_NON_EXISTING_PAYMENT_ID);
+    }
+
+    @Test
+    public void fetch_a_payment_if_it_exists() {
+        Payment expectedFoundPayment = aDummyPayment();
+        given(paymentRepository.findById(ANY_VALID_PAYMENT_ID)).willReturn(Optional.of(expectedFoundPayment));
+
+        Payment foundPayment = this.paymentService.findById(ANY_VALID_PAYMENT_ID);
+
+        verify(this.paymentRepository).findById((ANY_VALID_PAYMENT_ID));
+        assertThat(foundPayment, is(expectedFoundPayment));
+    }
+
+    @Test(expected = PaymentNotFoundException.class)
+    public void throw_PaymentNotFoundException_when_fetching_a_non_existing_payment() {
+        given(paymentRepository.findById(ANY_NON_EXISTING_PAYMENT_ID)).willReturn(Optional.empty());
+
+        this.paymentService.findById(ANY_NON_EXISTING_PAYMENT_ID);
+    }
+
+    @Test
+    public void return_empty_list_when_fetching_all_payments_if_none_exist() {
+        given(paymentRepository.findAll()).willReturn(Collections.emptyList());
+
+        List<Payment> allPayments = this.paymentService.findAll();
+
+        verify(this.paymentRepository).findAll();
+        assertThat(allPayments, is(empty()));
+    }
+
+    @Test
+    public void return_all_the_existing_payments() {
+        given(paymentRepository.findAll()).willReturn(Arrays.asList(aDummyPayment(), aDummyPayment()));
+
+        List<Payment> allPayments = this.paymentService.findAll();
+
+        verify(this.paymentRepository).findAll();
+        assertThat(allPayments, hasSize(2));
     }
 
     private void verityPaymentIdIsUUDI(Payment payment) {
