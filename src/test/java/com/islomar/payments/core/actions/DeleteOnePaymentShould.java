9a7ca87e162d734d42d1152ed048b6bf887e45a6
Isidro López <islomar@gmail.com>
2018-06-06 21:18:00 +0200

    [test] Test DeleteOnePayment action in isolation

---
+++ src/test/java/com/islomar/payments/core/actions/DeleteOnePaymentShould.java
@@ -0,0 +1,31 @@
+package com.islomar.payments.core.actions;
+
+import com.islomar.payments.core.model.PaymentService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import static com.islomar.payments.shared.ObjectMother.ANY_VALID_PAYMENT_ID;
+import static org.mockito.Mockito.verify;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+public class DeleteOnePaymentShould {
+
+    @Mock
+    private PaymentService paymentService;
+    private DeleteOnePayment deleteOnePayment;
+
+    @Before
+    public void setUp() {
+        initMocks(this);
+        this.deleteOnePayment = new DeleteOnePayment(paymentService);
+    }
+
+    @Test
+    public void delete_a_payment() {
+        this.deleteOnePayment.execute(ANY_VALID_PAYMENT_ID);
+
+        verify(this.paymentService).delete(ANY_VALID_PAYMENT_ID);
+    }
+
+}
