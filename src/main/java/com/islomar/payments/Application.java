d37bf0538eca608caaeb9cf1dba9bc4270bf46a3
Isidro López <islomar@gmail.com>
2018-06-09 18:57:58 +0200

    [cleanup] Delete unused imports

--- src/main/java/com/islomar/payments/Application.java
+++ src/main/java/com/islomar/payments/Application.java
@@ -4,9 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-
-import java.util.Arrays;
 
 @SpringBootApplication
 public class Application {
